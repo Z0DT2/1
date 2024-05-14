b6459c1541b457765c6c8268b4a504876ede8f9c
Z0DT2 <sergeysv527@gmail.com>
2024-05-15 00:09:35 +0300

    init commit

--- voice_assistant_bot-main/config.py
+++ voice_assistant_bot-main/config.py
@@ -1,4 +1,6 @@
-TELEGRAM_TOKEN = '6944234445:AAEeJSfaWeBKsO6Ulo7E-xZhBBWZy8llY0Y'
+import telebot
+
+telebot.TeleBot('6944234445:AAEeJSfaWeBKsO6Ulo7E-xZhBBWZy8llY0Y')
 FOLDER_ID = ''
 IAM_TOKEN = 't1.9euelZqJjMeTj8uNisqNjYuLl8vNne3rnpWamo2JksjJzZ3Pi57IzpqTm8fl9PcBZnBN-e9wGVLi3fT3QRRuTfnvcBlS4s3n9euelZrPkY3Mz4qKlp6bi5uWyJWPnu_8xeuelZrPkY3Mz4qKlp6bi5uWyJWPnr3rnpWazZeYzMibzcuTypCdkMaSnM213oac0ZyQko-Ki5rRi5nSnJCSj4qLmtKSmouem56LntKMng.dEXJC_tBdOOR2keGNQOqiweg8lV2zjdPRuemHWLFwTvGzvWmhIEIAVNP4aN2lVgbqbfaR7qIhWUjcsP_wjgYAA'
 COUNT_LAST_MSG = 4
